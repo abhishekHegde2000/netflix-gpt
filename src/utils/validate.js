a70ae01412be8642bedf8f8e3bc4edae01aa6940
abhishek Hegde <ahegde2021@gmail.com>
2023-10-16 10:16:04 +0530

    Update validate.js

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,8 +2,10 @@ export const checkValidData = (email, pasword) => {
   const isEmailvalid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
     email
   );
-  const isPasswordValid =
-    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(pasword);
+
+  
+  const isPasswordValid = /.*/.test(password);
+
 
   if (!isEmailvalid) {
     return "Email Id is not valid";
