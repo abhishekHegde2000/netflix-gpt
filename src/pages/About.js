c650449885e65ace9e1fb5dffc1d5acee1e5725d
abhishek Hegde <ahegde2021@gmail.com>
2023-11-02 22:52:49 +0530

    commit base url

--- src/pages/About.js
+++ src/pages/About.js
@@ -21,7 +21,7 @@ const About = () => {
       <div className='mb-8 text-gray-200'>
         Github: <a href='https://github.com/abhishekHegde2000/netflix-gpt' className='hover:text-blue-700 ml-3 text-blue-600 text-sm' target='_blank' rel='noreferrer'>NetflixGPT</a>
         <br />
-        Live Demo: <a href='https://8ttribute.com' target='_blank' className='hover:text-blue-700 ml-3 text-blue-600 text-sm' rel='noreferrer'>Live Demo</a>
+        Live Demo: <a href='https://gpt-stream-flix.netlify.app/' target='_blank' className='hover:text-blue-700 ml-3 text-blue-600 text-sm' rel='noreferrer'>Live Demo</a>
         <br></br>
         Linkedin: <a href='https://www.linkedin.com/in/abhishekhegde28/' className='hover:text-blue-700 ml-3 text-blue-600 text-sm' target='_blank' rel='noreferrer'>Abhishek Hegde</a>
       </div>
